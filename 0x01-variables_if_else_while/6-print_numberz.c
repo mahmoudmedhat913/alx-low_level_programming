4f6cb490fafdf2c2f612797c926a682bcbb731d1
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-07-14 07:58:09 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
