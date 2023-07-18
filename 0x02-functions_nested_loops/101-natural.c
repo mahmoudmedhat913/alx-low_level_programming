2d4b6eb610fd1a0b10f7ce12e4f540254753bf3e
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-07-18 07:57:54 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print the sum of all the multiplies of 3 and 5 below 1024 (excluded)
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0 ; num < 1024 ; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+		{
+			sum += num;
+		}
+
+	}
+	printf("%d\n", sum);
+	return (0);
+}
