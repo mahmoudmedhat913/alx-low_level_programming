37aa24f493d58be0edd9b2512bb9066848d1be9a
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-07-18 09:29:05 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -28,31 +28,31 @@ int num_length(int num)
 
 int main(void)
 {
-        int count, init;
-        unsigned long f1 = 1, f2 = 2, sum, max = 100000000;
-        unsigned long initf1 = 0, initf2 = 0, initsum = 0;
+	int count, init;
+	unsigned long f1 = 1, f2 = 2, sum, max = 100000000;
+	unsigned long initf1 = 0, initf2 = 0, initsum = 0;
 
-        for (count = 1 ; count <= 98 ; count++)
-        {
-                if (initf1 > 0)
-                        printf("%lu", initf1);
-                init = num_length(max) - 1 - num_length(f1);
-                while (initf1 > 0 && init > 0)
-                {
-                        printf("%d", 0);
-                        init--;
-                }
-                printf("%lu", f1);
-                sum = (f1 + f2) % max;
-                initsum = initf1 + initf2 + (f1 + f2) / max;
-                f1 = f2;
-                initf1 = initf2;
-                f2 = sum;
-                initf2 = initsum;
-                if (count != 98)
-                        printf(", ");
-                else
-                        printf("\n");
-        }
-        return (0);
+	for (count = 1 ; count <= 98 ; count++)
+	{
+		if (initf1 > 0)
+			printf("%lu", initf1);
+		init = num_length(max) - 1 - num_length(f1);
+		while (initf1 > 0 && init > 0)
+		{
+			printf("%d", 0);
+			init--;
+		}
+		printf("%lu", f1);
+		sum = (f1 + f2) % max;
+		initsum = initf1 + initf2 + (f1 + f2) / max;
+		f1 = f2;
+		initf1 = initf2;
+		f2 = sum;
+		initf2 = initsum;
+		if (count != 98)
+			printf(", ");
+		else
+			printf("\n");
+	}
+	return (0);
 }
