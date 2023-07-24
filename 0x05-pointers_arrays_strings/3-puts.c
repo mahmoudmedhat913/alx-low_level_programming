9701bc8b7a91df9dcbfd2ce9ebcacc6836f003d7
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-07-24 08:44:00 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _puts - Write a function that prints a string, followed by a new line, to stdout
+ * _puts - Write a function that prints a string,
+ * followed by a new line, to stdout
  * @str: strint to print
  */
 
