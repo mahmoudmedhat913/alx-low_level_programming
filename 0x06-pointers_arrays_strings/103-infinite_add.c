6ce6920806d6c99a325843ca0658f9e6c5923638
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-07-26 16:26:56 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,27 @@
 #include "main.h"
+/**
+ * rev_string - Reverse a string
+ * @s: The string to reverse
+ * Return: s
+ */
+char rev_string(char *s)
+{
+	int i, length;
+	char tmp;
+
+	length = 0;
+	for (i = 0; s[i] != '\0'; i++)
+		length++;
+
+	for (i = 0; i < length; i++)
+	{
+		tmp = s[i];
+		s[i] = s[length - 1];
+		s[length - 1] = tmp;
+		length--;
+	}
+	return (*s);
+}
 
 /**
  * *infinite_add - Write a function that adds two numbers.
