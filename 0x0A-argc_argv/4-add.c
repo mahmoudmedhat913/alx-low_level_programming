b2cf5572b1e1704006094cee34b075e6c46289a8
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-08-04 13:59:27 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,4 +12,47 @@
 
 int check_num(char *str)
 {
+	unsigned int count;
 
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - program that adds positive numbers.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count, num, sum;
+
+	sum = 0;
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			num = atoi(argv[count]);
+			sum += num;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
