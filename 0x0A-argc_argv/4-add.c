f1d68b4e10c4d64f749b79ca95c168ffb08f1e63
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-08-04 13:49:46 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check the string is digit
+ * @str: array str
+ * Return: 0
+ */
+
+int check_num(char *str)
+{
+
