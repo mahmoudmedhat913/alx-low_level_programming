4a43eb04ad6ae11a073d62ddf370c3af4b5d9c60
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-10-02 11:04:57 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -82,7 +82,7 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY || O_APPEND);
+		to = open(argv[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
 	free(buffer);
 	close_file(from);
