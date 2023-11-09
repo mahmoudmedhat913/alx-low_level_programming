774fa3f0fe70f93f71428336d6651deb140211f6
mahmoudmedhat913 <mahmoudmedhat913@gmail.com>
2023-11-09 05:02:41 -0800

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -28,7 +28,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	}
 	else
 	{
-		tmp->prev-next = tmp->next;
+		tmp->prev->next = tmp->next;
 		if (tmp->next != NULL)
 			tmp->next->prev = tmp->prev;
 	}
